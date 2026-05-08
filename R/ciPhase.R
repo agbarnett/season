a1bc724d80dfe502dd4f6f55ab85888231516580
njtierney <nicholas.tierney@gmail.com>
2026-05-08 12:38:45 +1000

    update ciPhase:
    * comments on own line
    * snake_case
    * explaining variable in if() clause
    * delete unused commented code
    * update return/list pattern

--- R/ciPhase.R
+++ R/ciPhase.R
@@ -27,39 +27,45 @@
 #' @examples
 #' \donttest{
 #' # 2000 normal samples, centred on zero
-#' theta <- rnorm(n=2000, mean=0, sd=pi/50)
-#' hist(theta, breaks=seq(-pi/8, pi/8, pi/30))
+#' theta <- rnorm(n = 2000, mean = 0, sd = pi / 50)
+#' hist(theta, breaks = seq(-pi / 8, pi / 8, pi / 30))
 #' ciPhase(theta)
 #' }
 #'
 #' @export ciPhase
 ciPhase <- function(theta, alpha = 0.05) {
-  thetac <- seq(0, 2 * pi, pi / 100) # proposed centres
-  m <- length(thetac)
-  d.theta <- vector(length = m, mode = 'numeric')
+  # proposed centres
+  theta_c <- seq(
+    from = 0,
+    to = 2 * pi,
+    by = pi / 100
+  )
+  m <- length(theta_c)
+  d_theta <- vector(length = m, mode = 'numeric')
   for (i in 1:m) {
-    d.theta[i] <- pi - mean(abs(pi - abs(theta - thetac[i]))) # Fisher page 36
+    # Fisher page 36
+    d_theta[i] <- pi - mean(abs(pi - abs(theta - theta_c[i])))
   }
-  centre <- thetac[d.theta == min(d.theta)]
+  centre <- theta_c[d_theta == min(d_theta)]
   if (length(centre) > 1) {
     centre <- centre[1]
   }
-  # plot(thetac,d.theta)
   # rotate data to be centred on pi
   # only rotate if centre is in top-half of the circle
   ideal <- theta
   diff <- 0
-  if (centre < pi / 2 || centre > 3 * pi / 2) {
+  theta_in_top_half <- centre < pi / 2 || centre > 3 * pi / 2
+  if (theta_in_top_half) {
     diff <- pi - centre
-    diffneg <- (-2 * pi) + diff
-    ideal <- theta + diff * (theta < pi) + diffneg * (theta > pi)
+    diff_neg <- (-2 * pi) + diff
+    ideal <- theta + diff * (theta < pi) + diff_neg * (theta > pi)
   }
-  toret <- list()
-  toret$mean <- mean(ideal) - diff
-  toret$lower <- as.numeric(stats::quantile(ideal, prob = alpha / 2) - diff)
-  toret$upper <- as.numeric(
-    stats::quantile(ideal, prob = 1 - (alpha / 2)) - diff
+
+  ret <- list(
+    mean = mean(ideal) - diff,
+    lower = as.numeric(stats::quantile(ideal, prob = alpha / 2) - diff),
+    upper = as.numeric(stats::quantile(ideal, prob = 1 - (alpha / 2)) - diff)
   )
-  # cat('Mean',mean,'Lower',lower,'Upper',upper,'\n')
-  return(toret)
+
+  ret
 }
