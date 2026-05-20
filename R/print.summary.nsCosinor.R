eefc142cb7f7f08003bad293c87606709c1588ea
njtierney <nicholas.tierney@gmail.com>
2026-05-20 22:18:57 +1000

    use check_if_class(x,"summary.nsCosinor")

--- R/print.summary.nsCosinor.R
+++ R/print.summary.nsCosinor.R
@@ -23,7 +23,7 @@
 #' }
 #' }
 print.summary.nsCosinor <- function(x, ...) {
-  check_if_nscosinor(x)
+  check_if_class(x, "summary.nsCosinor")
   k <- length(x$cycles)
   cat("Statistics for non-stationary cosinor based on MCMC chains\n")
   cat("Number of MCMC samples = ", x$niters - x$burnin + 1, "\n", sep = "", ...)
