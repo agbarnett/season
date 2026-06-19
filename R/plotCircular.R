5504d7364c43e6417e2c53a3d4039a30af90b383
Adrian Barnett <a.barnett@qut.edu.au>
2026-06-19 17:25:12 +1000

    update plotCircular to the ggplot format

--- R/plotCircular.R
+++ R/plotCircular.R
@@ -1,7 +1,8 @@
 # plotCircular.R
 # circular plot
 ## Changes
-## 7/2/10: incorporated titles/axes labekls/colour setting/auto legend
+## 7/2/10: incorporated titles/axes labels/colour setting/auto legend
+## June 2026: switch to ggplot
 
 #' Circular Plot Using Segments
 #'
@@ -9,376 +10,123 @@
 #'
 #' A circular plot can be useful for spotting the shape of the seasonal
 #' pattern. This function can be used to plot any circular patterns, e.g.,
-#' weekly or monthly. The number of segments will be the length of the variable
-#' `area1`.
+#' weekly or monthly. The method assumes that monthly data starts in January and weekly data starts on Monday.
 #'
 #' The plots are also called rose diagrams, with the segments then called
 #' "petals".
 #'
-#' @param area1 variable to plot, the area of the segments (or petals) are
+#' @param data Dataset to use for plot as a data.frame.
+#' @param time time variable in the data, typically month or day of the week.
+#' @param type type of data to plot, either 'monthly' or 'weekly', default:monthly.
+#' @param area variable(s) to plot, the area of the segments (or petals) are
 #' proportional to this variable.
-#' @param area2 2nd variable to plot (optional), the area of the segments are
-#' plotted in grey.
-#' @param spokes spokes that overlay segments, for example standard errors
-#' (optional).
-#' @param scale scale the overall size of the segments (default:0.8).
-#' @param labels optional labels to appear at the ends of the segments (there
-#' should be as many labels as there are `area1`).
-#' @param stats put area values at the ends of the segments, default:TRUE.
-#' @param dp decimal places for statistics, default=1.
 #' @param clockwise plot in a clockwise direction, default:TRUE.
 #' @param spoke.col spoke colour, default:black.
-#' @param lines add dotted lines to separate petals, default:FALSE.
-#' @param centrecirc controls the size of the circle at the centre of the plot,
-#' default:0.03.
 #' @param main title for plot, default:blank
 #' @param xlab x axis label, default:blank
 #' @param ylab y axis label, default:blank
 #' @param pieces.col colours for circular pieces, default:"white" for
 #' 1st and "grey" for second variable. Note that a list of available
 #' colours may be found with [colours()].
-#' @param length make the length of the segments proportional to the dependent
-#' variable, default:FALSE
 #' @param legend whether to include legend or not, default:TRUE when plotting
 #' two variables
-#' @param auto.legend list of parameters for legend, see [legend()]
-#' @param \dots additional arguments to [plot()] and/or
-#' [legend()]. See [par()] for more details
-#' @returns a circular plot, also known as "rosebud", and "nightingale" plots.
+#' @returns a circular plot in ggplot2 format, also known as "rosebud", and "nightingale" plots.
 #' @author Adrian Barnett \email{a.barnett@qut.edu.au}
 #' @references Fisher, N.I. (1993) *Statistical Analysis of Circular
 #' Data*. Cambridge University Press, Cambridge.
 #' @examples
-#' \donttest{
-#' weekfreq <- table(round(runif(100, min = 1, max = 7)))
-#' # weeks (random data)
-#' daysoftheweek <- c(
-#'   'Monday',
-#'   'Tuesday',
-#'   'Wednesday',
-#'   'Thursday',
-#'   'Friday',
-#'   'Saturday',
-#'   'Sunday'
-#' )
-#' plotCircular(area1 = weekfreq, labels = daysoftheweek, dp = 0)
-#' # Observed number of AFL players with expected values
-#' plotCircular(
-#'   area1 = AFL$players,
-#'   area2 = AFL$expected,
-#'   scale = 0.72,
-#'   labels = month.abb,
-#'   dp = 0,
-#'   lines = TRUE,
-#'   legend = FALSE
-#' )
-#' plotCircular(
-#'   area1 = AFL$players,
-#'   area2 = AFL$expected,
-#'   scale = 0.72,
-#'   labels = month.abb,
-#'   dp = 0,
-#'   lines = TRUE,
-#'   pieces.col = c("green", "red"),
-#'   auto.legend = list(labels = c("Obs", "Exp"), title = "# players"),
-#'   main = "Observed and Expected AFL players"
-#' )
-#' # months (dummy data)
-#' plotCircular(
-#'   area1 = seq(1, 12, 1),
-#'   scale = 0.7,
-#'   labels = month.abb,
-#'   dp = 0
-#' )
+#' ## Monthly numbers of AFL players
+#' AFL.frame = data.frame(AFL[c('month', 'players', 'expected')])
+#'aplot1 = plotCircular(
+#'  data = AFL.frame,
+#'  time = 'month',
+#'  areas = 'players',
+#'  legend = FALSE)
+#' ## The observed and expected number of players per month
+#' aplot = plotCircular(
+#' data = AFL.frame,
+#' time = 'month',
+#' areas = c('players', 'expected'))
+#' ## Creating weekly data as an example
+#'weekfreq <- data.frame(day = 1:7, counts = rpois(n = 7, lambda = 5))
+#'wplot = plotCircular(
+#'  data = weekfreq,
+#'  time = 'day',
+#'  type = 'weekly',
+#'  areas = 'counts',
+#'  legend = FALSE)
 #'
-#' }
 #' @export
-plotCircular <- function(
-  area1,
-  area2 = NULL,
-  spokes = NULL,
-  scale = 0.8,
-  labels,
-  stats = TRUE,
-  dp = 1,
+plotCircular = function(
+  data = NULL,
+  type = 'monthly',
+  time = NULL,
+  areas = NULL,
+  main = NULL,
+  xlab = NULL,
+  ylab = NULL,
   clockwise = TRUE,
-  spoke.col = 'black',
-  lines = FALSE,
-  centrecirc = 0.03,
-  main = "",
-  xlab = "",
-  ylab = "",
-  pieces.col = c("white", "gray"),
-  length = FALSE,
   legend = TRUE,
-  auto.legend = list(x = "bottomright", fill = NULL, labels = NULL, title = ""),
-  ...
+  spoke.col = NULL,
+  pieces.col = NULL
 ) {
-  ## NB: need some serious argumnt checking
-  ## NB2: can find list of available colours using colors()
-
-  ## No legend if only one variable and check area vars same length
-  if (is.null(area2)) {
-    legend <- FALSE
-  } else {
-    if (length(area1) != length(area2)) {
-      cat(
-        "Warning: length of",
-        deparse(substitute(area1)),
-        "and",
-        deparse(substitute(area2)),
-        "not equal\n"
-      )
-    }
+  # colours
+  if (is.null(pieces.col)) {
+    pieces.col = c('white', 'grey')
+  }
+  if (is.null(spoke.col)) {
+    spoke.col = 'black'
   }
 
-  ##print(pieces.col)
-  density.1 <- density.2 <- 0
-  if (pieces.col[1] != "white") {
-    density.1 <- NA
-  }
-  if (length(pieces.col) == 2) {
-    if (pieces.col[2] != "white") density.2 <- NA
-  }
+  # rename time variable
+  index = names(data) == time
+  names(data)[index] = 'id'
 
-  # the whole list of settable par's.
-  op <- par(no.readonly = TRUE)
-  # restore graphic settings whenever function exits
-  on.exit(par(op), add = TRUE)
+  # convert to long for multiple areas
+  long = pivot_longer(data, cols = all_of(areas), names_to = 'group') %>%
+    mutate(
+      id = id - 1, # make January zero
+      group = factor(group)
+    ) #
 
-  bins <- length(area1)
-  clockstart <- pi / 2 # default clock start at 12 o'clock
-  half <- 2 * pi / (bins * 2) # for moving text/spokes half-way round
-  if (clockwise) {
-    mult <- -1
-  } else {
-    mult <- 1
-  }
+  # circular plot
+  circle <- ggplot(data = long, aes(x = id, y = value, fill = group)) +
+    geom_bar(
+      width = 1,
+      stat = 'identity',
+      position = 'dodge',
+      col = spoke.col
+    ) +
+    scale_fill_manual(values = pieces.col) +
+    ggtitle(main) +
+    xlab(xlab) +
+    ylab(ylab) +
+    theme_bw() +
+    theme(panel.grid.minor = element_blank())
+  #  geom_text(data = daysoftheweek, aes(x=Var1, label = day))
 
-  ## First plot a circle (of radius 1) as a frame
-  detail <- 200 # number that controls graphical detail of cheeses
-  circle <- matrix(nrow = detail + 1, ncol = 2, data = 0)
-  frac <- 1 / detail
-  for (i in 1:(detail + 1)) {
-    circle[i, 1] <- 1 * cos(2 * pi * i * frac)
-    circle[i, 2] <- 1 * sin(2 * pi * i * frac)
-  }
-  plot(
-    circle,
-    type = 'l',
-    col = 'black',
-    bty = 'n',
-    yaxt = 'n',
-    main = main,
-    xlab = xlab,
-    ylab = ylab,
-    xlim = c(-1, 1),
-    ylim = c(-1, 1),
-    xaxt = 'n',
-    ...
-  )
-
-  ## scale cheeses to their area
-  aarea1 <- area1
-  if (!is.null(area2)) {
-    aarea2 <- area2
-  }
-  if (!length) {
-    aarea1 <- sqrt(area1 * 12 / pi)
-    if (!is.null(area2)) {
-      aarea2 <- sqrt(area2 * 12 / pi)
-    }
+  # if monthly
+  if (type == 'monthly') {
+    circle = circle +
+      scale_x_continuous(breaks = 0:11, labels = month.abb) +
+      coord_polar(theta = 'x', start = -2 * pi / 24) # offset
   }
 
-  ## scale the area to the maximum multiplied by the user-defined scale
-  ## draw the cheeses
-  for (cheeseno in 1:bins) {
-    if (is.null(area2)) {
-      scaled1 <- scale * aarea1 / max(aarea1)
-      cheese <- matrix(nrow = 102, ncol = 2, data = 0)
-      start <- 2 * pi * ((cheeseno - 1) / bins) + clockstart
-      frac <- 1 / 100
-      cheese[1, 1] <- centrecirc * mult * cos((2 * pi * frac / bins) + start)
-      cheese[1, 2] <- centrecirc * sin((2 * pi * frac / bins) + start)
-      cheese[102, 1] <- centrecirc *
-        mult *
-        cos((2 * pi * 100 * frac / bins) + start)
-      cheese[102, 2] <- centrecirc * sin((2 * pi * 100 * frac / bins) + start)
-      for (i in 1:100) {
-        cheese[i + 1, 1] <- mult *
-          scaled1[cheeseno] *
-          cos((2 * pi * i * frac / bins) + start)
-        cheese[i + 1, 2] <- scaled1[cheeseno] *
-          sin((2 * pi * i * frac / bins) + start)
-      }
-      polygon(
-        cheese,
-        density = density.1,
-        angle = 0,
-        lty = 1,
-        lwd = 1,
-        border = "black",
-        col = pieces.col[1]
-      )
-    }
-
-    ## plot with two segments #
-    ## 1st pattern
-    if (!is.null(area2)) {
-      allarea <- c(aarea1, aarea2)
-      scaled1 <- scale * aarea1 / max(allarea)
-      scaled2 <- scale * aarea2 / max(allarea)
-      cheese1 <- matrix(nrow = 52, ncol = 2, data = 0)
-      cheese2 <- matrix(nrow = 52, ncol = 2, data = 0)
-      start <- 2 * pi * ((cheeseno - 1) / bins) + clockstart
-      frac <- 1 / 100
-
-      ## centrecirc: do not start at c(0,0) to prevent a dense block
-      cheese1[1, 1] <- centrecirc *
-        mult *
-        cos((2 * pi * 0 * frac / bins) + start)
-      cheese1[1, 2] <- centrecirc * sin((2 * pi * 0 * frac / bins) + start)
-      cheese1[52, 1] <- centrecirc *
-        mult *
-        cos((2 * pi * 51 * frac / bins) + start)
-      cheese1[52, 2] <- centrecirc * sin((2 * pi * 51 * frac / bins) + start)
-      for (i in 1:50) {
-        cheese1[i + 1, 1] <- mult *
-          scaled1[cheeseno] *
-          cos((2 * pi * i * frac / bins) + start)
-        cheese1[i + 1, 2] <- scaled1[cheeseno] *
-          sin((2 * pi * i * frac / bins) + start)
-      }
-      polygon(
-        cheese1,
-        density = density.1,
-        angle = 0,
-        lty = 1,
-        lwd = 1,
-        border = "black",
-        col = pieces.col[1]
-      )
-
-      ## 2nd pattern
-      start <- 2 * pi * ((cheeseno - 1) / bins) + clockstart
-      cheese2[1, 1] <- centrecirc *
-        mult *
-        cos((2 * pi * 50 * frac / bins) + start)
-      cheese2[1, 2] <- centrecirc * sin((2 * pi * 50 * frac / bins) + start)
-      cheese2[52, 1] <- centrecirc *
-        mult *
-        cos((2 * pi * 100 * frac / bins) + start)
-      cheese2[52, 2] <- centrecirc * sin((2 * pi * 100 * frac / bins) + start)
-      for (i in 51:100) {
-        cheese2[i + 1 - 50, 1] <- mult *
-          scaled2[cheeseno] *
-          cos((2 * pi * i * frac / bins) + start)
-        cheese2[i + 1 - 50, 2] <- scaled2[cheeseno] *
-          sin((2 * pi * i * frac / bins) + start)
-      }
-      polygon(
-        cheese2,
-        density = density.2,
-        angle = 0,
-        lty = 1,
-        lwd = 1,
-        border = "black",
-        col = pieces.col[2]
-      )
-    }
-  }
-  ## add the text
-  if (!is.null(labels) && !stats) {
-    for (cheeseno in 1:bins) {
-      x <- mult * 0.92 * cos((2 * pi * cheeseno / bins) + start + half)
-      y <- 0.92 * sin((2 * pi * cheeseno / bins) + start + half)
-      text(x, y, labels[cheeseno])
-    }
+  # if weekly
+  if (type == 'weekly') {
+    days = c('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')
+    circle = circle +
+      scale_x_continuous(breaks = 0:6, labels = days) +
+      coord_polar(theta = 'x', start = -2 * pi / 14) # offset
   }
 
-  ## add the labels with stats
-  if (!is.null(labels) && stats) {
-    clabel2 <- formatC(area1, format = "f", digits = dp) # convert to character
-    for (cheeseno in 1:bins) {
-      x <- mult * 0.86 * cos((2 * pi * cheeseno / bins) + start + half)
-      y <- 0.86 * sin((2 * pi * cheeseno / bins) + start + half)
-      label1 <- labels[cheeseno]
-      label <- paste(label1, "\n", clabel2[cheeseno])
-      text(x, y, label)
-    }
+  # reverse direction option
+  if (clockwise == FALSE) {
+    circle = circle + coord_polar(theta = 'x', direction = -1)
   }
-
-  ## add spokes representing uncertainty
-  if (!is.null(spokes)) {
-    scaleds <- scale * spokes / max(spokes)
-    halfcheese <- (2 * pi) / (bins * 2)
-    for (cheeseno in 1:bins) {
-      spokes <- matrix(data = 0, nrow = 2, ncol = 2)
-      spokes[1, 1] <- centrecirc *
-        mult *
-        scaleds[cheeseno] *
-        cos((2 * pi * cheeseno / bins) + start + half)
-      spokes[1, 2] <- centrecirc *
-        scaleds[cheeseno] *
-        sin((2 * pi * cheeseno / bins) + start + half)
-      spokes[2, 1] <- mult *
-        scaleds[cheeseno] *
-        cos((2 * pi * cheeseno / bins) + start + half)
-      spokes[2, 2] <- scaleds[cheeseno] *
-        sin((2 * pi * cheeseno / bins) + start + half)
-      lines(spokes, pch = 0, type = 'l', col = spoke.col, lty = 1, lwd = 1.5)
-    }
-  } # end of spokes
-
-  ## add dotted lines to separate months
-  if (lines) {
-    halfcheese <- (2 * pi) / (bins * 2)
-    for (cheeseno in 1:bins) {
-      breaks <- matrix(data = 0, nrow = 2, ncol = 2)
-      breaks[1, 1] <- centrecirc * cos((2 * pi * cheeseno / bins) + start)
-      breaks[1, 2] <- centrecirc * sin((2 * pi * cheeseno / bins) + start)
-      breaks[2, 1] <- cos((2 * pi * cheeseno / bins) + start)
-      breaks[2, 2] <- sin((2 * pi * cheeseno / bins) + start)
-      lines(breaks, pch = 0, type = 'l', lty = 3, lwd = 1)
-    }
-  } # end of lines
-
-  ## add legend if set
-  if (legend) {
-    if (length(auto.legend$x) == 0) {
-      legend.x <- "bottomright"
-    } else {
-      legend.x <- auto.legend$x
-    }
-
-    ##if (length(auto.legend$title)==0) {
-    ## title <- NULL
-    ##} else {
-    ## title <- auto.legend$title
-    ##}
-
-    if (length(auto.legend$labels) == 0) {
-      labels <- c(deparse(substitute(area1)), deparse(substitute(area2)))
-    } else {
-      labels <- auto.legend$labels
-    }
-
-    if (length(auto.legend$fill) == 0) {
-      fill <- pieces.col
-    } else {
-      fill <- auto.legend$fill
-    }
-
-    ##print(legend.x)
-    ##print(labels)
-    ##print(fill)
-    ##print(title)
-    legend(
-      x = legend.x,
-      legend = labels,
-      title = auto.legend$title,
-      fill = fill,
-      ...
-    )
+  # legend
+  if (legend == FALSE) {
+    circle = circle + theme(legend.position = 'none')
   }
+  return(circle)
 }
